698d02ba71f2dcbd3794de39741eb2060408dd42
andrii <andrijostrovskij1@gmail.com>
2024-07-16 19:40:29 +0300

    fixed exeption

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Http\Middleware\Auth;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -18,12 +19,14 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (Exception $e, Request $request) {
-
             if ($request->is('api/*')) {
+                $status = $e instanceof \Symfony\Component\HttpKernel\Exception\HttpException
+                    ? $e->getStatusCode()
+                    : Response::HTTP_INTERNAL_SERVER_ERROR;
+
                 return response()->json([
                     'message' => $e->getMessage(),
-                    'error' => "Line " . $e->getLine() . " - " . $e->getFile()
-                ], 503);
+                ], $status);
             }
         });
     })->create();
